fb3ee571239d75e96cbba5aba72ea24fb7468c4f
Hugo Mallet <hugomallet06@gmail.com>
2013-03-13 02:05:11 +0100

    Fix CORS allowed methods

--- api/policies/cross-domain.js
+++ api/policies/cross-domain.js
@@ -3,6 +3,7 @@
 */
 module.exports = function (req,res,ok) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type");
     ok();
 };
